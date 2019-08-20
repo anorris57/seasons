8031ccf39de74bf42b5bf271ab41b7e52bf913fe
Angela Norris <cocoanorris@yahoo.com>
2019-08-20 10:16:27 -0400

    setting state

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  //constructor automactically called
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props){
+    super(props);//reference to parent component because over writing
+    this.state = { lat: null };//null don't know yet what number is
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err)
     );
-    return <div>Latitude:</div>
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
