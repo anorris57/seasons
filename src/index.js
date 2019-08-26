8f713218c064ea6ee6332fa8f2e1f6b84d07abf7
Angela Norris <cocoanorris@yahoo.com>
2019-08-26 14:15:38 -0400

    classes, states, life cycle methods, config object

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   //constructor called optionallly and automactically first if you want to use it
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props){
-    super(props);//reference to parent component because over writing
-    this.state = { lat: null, errorMessage: '' };//null don't know yet what number is
+    state = { lat: null, errorMessage: '' };
 
+  componentDidMount (){
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
+  renderContent(){
     if (this.state.errorMessage && !this.state.lat){
       return <div>Error: {this.state.errorMessage}</div>
     }
     
     if (!this.state.errorMessage && this.state.lat){
-      return <div> Latitude: {this.state.lat} </div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>
+    return <Spinner message="Please accept location request."/>
+
+  }
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
